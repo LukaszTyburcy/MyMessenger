69c9413383df255b28fb293a3e136634a31c404e
lukasz1305 <lukasz.tyburcy@gmail.com>
2017-12-22 14:18:35 +0100

    Add background

--- app/src/main/java/com/lukasz/mymessenger/MainActivity.kt
+++ app/src/main/java/com/lukasz/mymessenger/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.lukasz.mymessenger
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
