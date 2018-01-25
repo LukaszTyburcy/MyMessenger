12b78b10a06d27899a7c3ca593b546d4f5e7a12b
lukasz1305 <lukasz.tyburcy@gmail.com>
2018-01-25 20:47:57 +0100

    Add toolbar

--- app/src/main/java/com/lukasz/mymessenger/ChatActivity.kt
+++ app/src/main/java/com/lukasz/mymessenger/ChatActivity.kt
@@ -1,28 +1,38 @@
 package com.lukasz.mymessenger
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_chat.*
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 
 /**
  * Created by Lukasz on 2017-12-22.
 Upload Picture
  */
-class ChatActivity : Activity(){
+class ChatActivity : AppCompatActivity(){
+
+    private lateinit var mApp: MessengerApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mApp = application as MessengerApplication
         setContentView(R.layout.activity_chat)
+        mApp = application as MessengerApplication
+    }
 
-        logoutBTN.setOnClickListener{
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if(id == R.id.logout){
             mApp.mAuth.signOut()
             finish()
             startActivity(Intent(this,MainActivity::class.java))
         }
+        return super.onOptionsItemSelected(item)
     }
-
-
 }
 
